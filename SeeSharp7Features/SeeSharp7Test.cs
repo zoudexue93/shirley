25aea9cb3157ed7264119ab55e4554164c0aa91d
Patrick Zenhäusern <patrick.zenhaeusern@colygon.com>
2017-07-26 12:42:51 +0200

    Add project files.

---
+++ SeeSharp7Features/SeeSharp7Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeeSharp7Features
+{
+  public class SeeSharp7Test
+  {
+    public void MySeeSharp7Method()
+    {
+      var dict = new Dictionary<string, string> {{"test", "value"}};
+      dict.TryGetValue("test", out string value);
+
+      Console.WriteLine(value);
+      Console.ReadLine();
+    }
+  }
+}
---
+++ VS2017_O365_VSTSWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2017_O365_VSTSWeb.MvcApplication" Language="C#" %>
